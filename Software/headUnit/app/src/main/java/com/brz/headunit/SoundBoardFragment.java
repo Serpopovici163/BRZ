9bcccc9ccccbdd71dd87d7737ec5558bc1ee3e9a
Serban Popovici <serpopovici163@gmail.com>
2022-08-22 03:42:47 -0400

    backup aug 22, worked on various fragments

--- Software/headUnit/app/src/main/java/com/brz/headunit/SoundBoardFragment.java
+++ Software/headUnit/app/src/main/java/com/brz/headunit/SoundBoardFragment.java
@@ -2,13 +2,18 @@ package com.brz.headunit;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class SoundBoardFragment extends Fragment {
+
+    private TextView selectedSoundTextView;
 
     public SoundBoardFragment() {
         // Required empty public constructor
@@ -23,5 +28,18 @@ public class SoundBoardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sound_board, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        selectedSoundTextView = requireView().requireViewById(R.id.selected_sound_textview);
+
+    }
+
+    //get list of available sounds from arduino horn manager and generate as many buttons as necessary
+    void generateSoundButton() {
+
     }
 }
---
+++ Software/headUnit/app/src/main/java/com/brz/headunit/services/MaintenanceService.java
@@ -0,0 +1,9 @@
+package com.brz.headunit.services;
+
+//Extends DiagnosticService but kept separate for simplicity
+public class MaintenanceService {
+
+    //keep track of car fluid changes and warn driver at selected intervals
+    //datetime operations
+
+}
